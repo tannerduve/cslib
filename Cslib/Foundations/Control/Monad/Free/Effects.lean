c56a44aa0c92d10e2bb37ac29530e8f1e20010f7
Tanner Duve <tannerduve@gmail.com>
2025-09-08 13:39:29 -0700

    Move Control/Monad to Foundations/Control/Monad and example to tests

--- Cslib/Foundations/Control/Monad/Free/Effects.lean
+++ Cslib/Foundations/Control/Monad/Free/Effects.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Tanner Duve
 -/
 import Mathlib.Control.Monad.Cont
-import Cslib.Control.Monad.Free
+import Cslib.Foundations.Control.Monad.Free
 import Mathlib.Control.Monad.Writer
 
 /-!
--- Cslib/Foundations/Control/Monad/Free/Fold.lean
+++ Cslib/Foundations/Control/Monad/Free/Fold.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Tanner Duve. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Tanner Duve
 -/
-import Cslib.Control.Monad.Free
+import Cslib.Foundations.Control.Monad.Free
 
 /-!
 # Free Monad Catamorphism
