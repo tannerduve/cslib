cb45d51d527b9e414d93439083f00245432bad9a
Kim Morrison <477956+kim-em@users.noreply.github.com>
2025-10-15 21:28:49 +1100

    chore: run `lake exe mk_all` (#107)
    This updates `Cslib.lean`, so `import Cslib` has the desired effect.

---
+++ CslibTests.lean
@@ -0,0 +1,8 @@
+import CslibTests.Bisimulation
+import CslibTests.CCS
+import CslibTests.DFA
+import CslibTests.FreeMonad
+import CslibTests.HasFresh
+import CslibTests.LTS
+import CslibTests.LambdaCalculus
+import CslibTests.ReductionSystem
