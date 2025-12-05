a5389ab2b3ab428a54d889be8caa3c11dc0ef152
Tanner Duve <tannerduve@gmail.com>
2025-12-05 15:16:06 -0800

    docs

--- Cslib/Foundations/Control/Monad/Time.lean
+++ Cslib/Foundations/Control/Monad/Time.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Tanner Duve. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Tanner Duve
+Authors: Tanner Duve, Sorrachai Yingchareonthawornhcai
 -/
 
 import Mathlib.Control.Monad.Writer
