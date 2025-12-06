9381b9aab11a148c73313efbd2bcf3c332c61c5b
Shreyas <shreyasss94@gmail.com>
2025-12-06 01:36:12 +0100

    Test PR to PR ability with doc comment

--- Cslib/Algorithms/QueryModel.lean
+++ Cslib/Algorithms/QueryModel.lean
@@ -11,7 +11,8 @@ import Cslib.Foundations.Control.Monad.Time
 /-
 # Query model
 
-This file defines a simple query language modeled as a free monad over primitive query operations.
+This file defines a simple query language modeled as a free monad over a
+parametric type of  query operations.
 
 We equip this language with a cost model (`TimeM`) that counts how many primitive queries
 are performed. An example algorithm (merge sort) is implemented in
