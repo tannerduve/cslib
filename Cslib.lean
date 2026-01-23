d8ca51bc1e80af3dce5bbf35fe7d98df4617b926
Bolton Bailey <bolton.bailey@gmail.com>
2026-01-22 18:37:08 -0800

    feat: add time step counting for relations (#270)
    This PR adds `Reduction.relates{In,Within}Steps`, functions that
    communicate whether a relation forms a directed path of length (at most)
    `n` between two elements.
    
    Specifically, it includes:
    
    * Definitions of these functions
    * Theorems about reflexivity and transitivity
    * Theorems that track the growth of ancillary functions with respect to
    the relation
    
    These are a prerequisite to defining time bounds, perhaps for multiple
    models of computation, but in particular for Turing machines, see PR
    #269.
    
    ---------
    
    Co-authored-by: Chris Henson <chrishenson.net@gmail.com>

--- Cslib.lean
+++ Cslib.lean
@@ -36,6 +36,7 @@ public import Cslib.Foundations.Data.OmegaSequence.Flatten
 public import Cslib.Foundations.Data.OmegaSequence.InfOcc
 public import Cslib.Foundations.Data.OmegaSequence.Init
 public import Cslib.Foundations.Data.OmegaSequence.Temporal
+public import Cslib.Foundations.Data.RelatesInSteps
 public import Cslib.Foundations.Data.Relation
 public import Cslib.Foundations.Lint.Basic
 public import Cslib.Foundations.Semantics.FLTS.Basic
